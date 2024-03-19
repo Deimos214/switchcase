aa85dba40bd422b41dbd2a7565b00351fa0368d4
Usuario <Usuario@CPD03-M01>
2024-03-19 10:41:36 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a opção A, B ou C.");
+            string opcao = Console.ReadLine().ToUpper();
+
+            switch(opcao)
+            {
+                case "A":
+                    Console.WriteLine("Opção selecionada foi A.");
+                    break;
+                case "B":
+                    Console.WriteLine("Opção selecionada foi B.");
+                    break;
+                case "C":
+                    Console.WriteLine("Opção selecionada foi C.");
+                    break;
+                default: Console.WriteLine("Opção inválida.");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
